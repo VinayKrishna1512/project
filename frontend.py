cfb75a1a8ec4aa99b7b6e976f7eb16796e6cc66c
VinayKrishna1512 <135038094+VinayKrishna1512@users.noreply.github.com>
2023-06-03 10:52:42 +0530

    Update frontend.py
    made a few changes to adjust to the change in dataset

--- frontend.py
+++ frontend.py
@@ -13,18 +13,18 @@ teams = [
 
 cities = [
          "Chennai", "Mumbai", "Ahmedabad", "Lucknow", "Jaipur", "Bengaluru",
-         "Mohali", "Kolkata", "Dharamsala", "Guwahati", "Delhi", "Hyderabad"
+          "Kolkata", "Dharamsala", "Guwahati", "Delhi", "Hyderabad"
          ]
 
 column1, column2 = st.beta_columns(2)
 
 with column1:
-    batting_team = st.selectbox("Select the batting team", sorted(teams))
+    BattingTeam = st.selectbox("Select the batting team", sorted(teams))
 
 with column2:
-    bowling_team = st.selectbox("Select the bowling team", sorted(teams))
+    BowlingTeam = st.selectbox("Select the bowling team", sorted(teams))
 
-select_city = st.selectbox("Select Host city", sorted(cities))
+City = st.selectbox("Select Host city", sorted(cities))
 target = st.number_input("Target")
 
 column3, column4, column5 = st.beta_columns(3)
@@ -41,24 +41,24 @@ with column5:
 if st.button('Predict Probability'):
     runs_left = target - score
     balls_left = 120 - (overs * 6)
-    wickets = 10 - wickets
-    crr = score / overs
-    rrr = (runs_left * 6) / balls_left
+    wickets_left = 10 - wickets
+    current_run_rate = score / overs
+    required_run_rate = (runs_left * 6) / balls_left
 
     input_df = pd.DataFrame({
-        'batting_team': [batting_team],
-        'bowling_team': [bowling_team],
-        'city': [select_city],
+        'BattingTeam': [BattingTeam],
+        'BowlingTeam': [BowlingTeam],
+        'City': [City],
         'runs_left': [runs_left],
         'balls_left': [balls_left],
-        'wickets': [wickets],
-        'total_runs_x': [target],
-        'crr': [crr],
-        'rrr': [rrr]
+        'wickets_left': [wickets_left],
+        'target': [target],
+        'current_run_rate': [current_run_rate],
+        'required_run_rate': [required_run_rate]
     })
 
     result = pipe.predict_proba(input_df)
     loss = result[0][0]
     win = result[0][1]
-    st.header(batting_team + " - " + str(round(win * 100)) + "%")
-    st.header(bowling_team + " - " + str(round(loss * 100)) + "%")
+    st.header(BattingTeam + " - " + str(round(win * 100)) + "%")
+    st.header(BowlingTeam + " - " + str(round(loss * 100)) + "%")
