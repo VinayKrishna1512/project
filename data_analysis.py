33b67b54d198422cd161e31e93bb227d3a03bc90
vaibhavgiri4114 <133363420+vaibhavgiri4114@users.noreply.github.com>
2023-06-04 11:56:41 +0530

    Update data_analysis.py
    added the other 3 functions

--- data_analysis.py
+++ data_analysis.py
@@ -53,4 +53,51 @@ def win_tosswin():
     plt.ylabel("Various Teams Playing in IPL", fontsize =21)
     plt.title("Horizontal Bar Chart For percent win for the team when it won the toss" , fontsize = 20)
     plt.show()
+# 4 Finding the top 15 player of the match players and plotting a chart
+def top_15_potm():
+    player = matches['player_of_match']
+    from collections import Counter
+    count = Counter(player).values()
+    list1 = pd.DataFrame({'Player' :matches['player_of_match'].unique()})
+    list1['count'] = count
+    list1 = list1[list1['count'] > 8].sort_values(by = 'count', ascending = False)
+    #plotting the Doughnut Chart
+
+    plt.rcParams["figure.figsize"][0] = 8
+    plt.rcParams["figure.figsize"][1] = 8
+    plt.pie(list1['count'], labels = list1['Player'], wedgeprops = {'linewidth' : 7, 'edgecolor' : 'white'})
+    my_circle = plt.Circle(( 0,0) , 0.7 , color = 'White')
+    p = plt.gcf()
+    p.gca().add_artist(my_circle)
+    plt.title("Doughnut Chart for top 15 Cricketers who have been Man of the Matches", fontsize= 18)
+    plt.show()
+
+
+# 5 Getting The Scores of All Team in each Season in IPL
+def score_all_teams():
+    s = deliveries.match_id.replace(matches.set_index('id')['season'])
+    deliveries['season'] = s
+    # print(deliveries['season'].unique())
+
+    fig, ax = plt.subplots(figsize=(16,10))
+    deliveries_by_season = deliveries.groupby(['season' , 'batting_team']).sum()
+    deliveries_by_season['batsman_runs'].unstack().plot(kind = 'bar',ax = ax, width = 0.4, fontsize = 16)
+    plt.title("Runs Scored by Teams In The Seasons of IPL" , fontsize = 25)
+    plt.ylabel("Runs Scored By Teams" ,fontsize = 20)
+    plt.xlabel("Seasons Of IPL", fontsize = 20)
+    ax.legend(loc='center left', bbox_to_anchor=(1, 1))
+    plt.show()
+
+# 6 Getting The Wickets of All Team in each Season in IPL
+def wickets_all_teams():
+    deliveries['player_dismissed_no'] = deliveries['player_dismissed']
+    deliveries['player_dismissed_no'] = np.where(deliveries['player_dismissed_no'].isnull(), 0 , 1)
+    wickets_by_season = deliveries.groupby(['season' , 'bowling_team']).sum()
+    fig, ax = plt.subplots(figsize=(16,10))
+    wickets_by_season['player_dismissed_no'].unstack().plot(kind = 'bar',ax = ax, width = 0.4, fontsize = 16)
+    plt.title("Wickets Taken by Teams In The Seasons of IPL" , fontsize = 25)
+    plt.ylabel("Wickets Taken By Teams" ,fontsize = 20)
+    plt.xlabel("Seasons Of IPL", fontsize = 20)
+    ax.legend(loc='center left', bbox_to_anchor=(1, 1))
+    plt.show()
     
